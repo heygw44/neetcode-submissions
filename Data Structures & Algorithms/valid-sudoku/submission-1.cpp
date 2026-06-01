daef06ba67742a1f1d5b86214749c6291b8fd7fb
ParkGunWoo <heygw44@gmail.com>
2026-06-01 14:13:21 +0900

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<unordered_set<char>> row(9), col(9), box(9);
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                char val = board[r][c];
+                if (val == '.') continue;
+                
+                int b = (r/3) * 3 + (c/3);
+
+                if (row[r].count(val) || col[c].count(val) || box[b].count(val)) {
+                    return false;
+                }
+                row[r].insert(val);
+                col[c].insert(val);
+                box[b].insert(val);
+            }
+        }
+        return true;
+    }
+};
